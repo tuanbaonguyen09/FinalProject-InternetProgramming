0d1989da09730a1a96b39832bffaed447a814405
zune <tuanbaonguyen09@gmail.com>
2023-08-17 11:44:01 +0700

    Add Login Feature v0.1

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,97 +1,102 @@
 import * as React from 'react'
 import './Login.css'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 // Axios
 import axios from 'axios';
 
-export default class Login extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            db_accounts: [],
-            value : '',
-            emailInput:'',
-            passwordInput:'',
-            emailCheck :'',
-            passwordCheck: '',
+export default function Login(){
+    const navigate = useNavigate()
+    const [isLogin, setIsLogin] = React.useState(false)
+
+    const [email, setEmail] = React.useState()
+    const [emailErr, setEmailErr] = React.useState(true)
+
+    const [password, setPassword] = React.useState()
+    const [passwordErr, setPasswordErr] = React.useState(true)
+
+    axios.defaults.withCredentials = true;
+
+    React.useEffect(() => {
+      axios.get('/api/login').then((response) => {
+        if(response.data.loggedIn == true){
+            setIsLogin(true)
         }
-        this.handleEmailChange = this.handleEmailChange.bind(this)
-        this.handlePasswordChange = this.handlePasswordChange.bind(this)
-        this.handleFormSubmit = this.handleFormSubmit.bind(this)
-    }
+      })
+    }, [])
 
-    componentDidMount(){
-        axios.get('/api/accounts').then(result => {
-          this.setState({db_accounts:  result.data.accounts})
-        }).catch(error => console.log(error))
-    }   
-
-    handleFormSubmit(event){
-        event.preventDefault();
-        //Handle Login Successful
-        this.state.db_accounts.forEach( (item, index) => {
-            if(item.email == this.state.emailInput && item.password == this.state.passwordInput && this.state.emailCheck.length < 1 && this.state.passwordCheck < 1){
-                console.log('Successful')
+    const handleFormSubmit = (event) => {
+        event.preventDefault()
+        if(emailErr || passwordErr){
+            alert("Vui lòng nhập đúng thông tin đăng nhập")
+            return
+        }
+        const configuration = {
+            method: "post",
+            url: "/api/login",
+            data: {
+                email : email,
+                password: password,
+              },
+        }
+        axios(configuration).then(response => {
+            if(response.data.message){
+                alert(response.data.message)
+            }else {
+                alert('Đăng nhập thành công')
+                setIsLogin(true)
             }
-        })
-
+        }).catch(error => console.log(error))
     }
 
-    handleEmailChange(event){
-        this.setState({emailInput:event.target.value ,emailCheck: this.emailValidation(event.target.value)})
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value)
+        if(passwordValidation(event.target.value)) setPasswordErr(false)
+        else setPasswordErr(true)
     }
 
-    emailValidation(input){
+    const passwordValidation = (value) => {
+        return (value.length >= 5)
+    }
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value)
+        if(emailValidation(event.target.value)) setEmailErr(false)
+        else setEmailErr(true)
+    }
+    const emailValidation = (value) => {
         const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
-        if(regex.test(input)) {
-            return "";
-        }
-        return "Vui lòng nhập email hợp lệ";
+        return value.match(regex)
     }
-
-    handlePasswordChange(event){
-        this.setState({passwordInput:event.target.value, passwordCheck: this.passwordValidation(event.target.value)})
-    }
-
-    passwordValidation(input){
-        if(input.length  >= 5) {
-            return "";
-        }
-        return "Vui lòng nhập password hợp lệ";
-    }
-
-    render(){
-        return(
-            <div className="Login w-screen h-screen flex">
-                <div className="LoginInner w-full ">
-                    <div className="LoginWindow">
-                        <h1>Please Login</h1>
-                        <form className="LoginForm" action="">
-                            <div className="item">
-                                <label htmlFor="email">Email</label>
-                                <input type="email" name="email" id="email" placeholder="Email" onChange={this.handleEmailChange} />
-                                <div className="validation" id="email_validation">{this.state.emailCheck}</div>
-                            </div>
-                            <div className="item">
-                                <label htmlFor="password">Password</label>
-                                <input type="password" name="password" id="password" placeholder="Password"  onChange={this.handlePasswordChange}/>
-                                <div className="validation" id="password_validation">{this.state.passwordCheck}</div>
-                            </div>
-                            <button className="LoginButton" onClick={this.handleFormSubmit}>
-                                Log in
-                            </button>
-                            <div className="flex gap-2 items-center text-[13px]">
-                                <div className="opacity-40">Don’t have an account?</div>
-                                <NavLink to="/register">Sign Up</NavLink>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div className="Wallpaper w-full bg-blue-800">
-    
+    return(
+        <div className="Login w-screen h-screen flex">
+            <div className="LoginInner w-full ">
+                <div className="LoginWindow">
+                    <h1>Please Login</h1>
+                    <h1>{isLogin ? "Welcome" : ""}</h1>
+                    <form className="LoginForm" action="">
+                        <div className="item">
+                            <label htmlFor="email">Your Email</label>
+                            <input type="email" name="email" id="email" placeholder="Email" onChange={handleEmailChange} />
+                            <div className="validation" id="email_validation">{emailErr ? "Vui lòng nhập email hợp lệ":""}</div>
+                        </div>
+                        <div className="item">  
+                            <label htmlFor="password">Your Password</label>
+                            <input type="password" name="password" id="password" placeholder="Password"  onChange={handlePasswordChange}/>
+                            <div className="validation" id="password_validation">{passwordErr ? "Vui lòng nhập mật khẩu hợp lệ":""}</div>
+                        </div>
+                        <button className="LoginButton" onClick={handleFormSubmit}>
+                            Log in
+                        </button>
+                        <div className="flex gap-2 items-center text-[13px]">
+                            <div className="opacity-40">Don’t have an account?</div>
+                            <NavLink to="/register">Sign Up</NavLink>
+                        </div>
+                    </form>
                 </div>
             </div>
-        )
-    }
+            <div className="Wallpaper w-full bg-blue-800">
 
+            </div>
+        </div>
+    )
 }
--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const mysql = require('mysql')
-require("dotenv").config();
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
 const app = express()
 const bodyParser = require('body-parser');
+require("dotenv").config();
+
+app.use(cookieParser())
+
+app.use(
+  session({
+    key: "userId",
+    secret: "subscribe",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 60 * 60 * 24,
+    },
+  })
+);
+
+
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
@@ -26,12 +45,11 @@ var con = mysql.createConnection({
  });
 
 
- app.post('/api/register', (req, res, next) => {
+  app.post('/api/register', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   con.query('SELECT * FROM accounts WHERE email = ? AND password = ?', [email, password], function(err, result) {
       if (err) throw err;
-        // if user not found
         if (result.length <= 0) {
           con.query('INSERT INTO accounts (email, password) VALUES (?, ?)', [email, password],function(err, result) {
             if (err) throw err;
@@ -43,10 +61,29 @@ var con = mysql.createConnection({
     })
   })
   
-app.post('/api/login', (req, res) => {
-  var sql = "SELECT * FROM accounts";
-  con.query(sql, function(err, results) {
-    if (err) throw err;
-    res.json({news: results});
-  });
-});
+  app.post('/api/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    con.query("SELECT * FROM accounts WHERE email = ?", email, (err, result) => {
+        if (err) throw err;
+        if (result.length > 0) {
+          if (password == result[0].password){
+              req.session.user = result;
+              res.send(result);
+            } else {
+              res.json({ message: "Sai mật khẩu" });
+            }
+        } else {
+          res.json({ message: "Tài khoản không tồn tại" });
+        }
+      }
+    )
+  })
+
+  app.get('/api/login', (req, res) => {
+    if(req.session.user){
+      res.json({loggedIn: true, user: req.session.user})
+    }else {
+      res.json({loggedIn: false})
+    }
+  })
