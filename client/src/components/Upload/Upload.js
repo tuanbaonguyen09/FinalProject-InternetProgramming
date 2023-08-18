2316d14f3d58531a7e481667bfb51a4bee19dcea
zune <tuanbaonguyen09@gmail.com>
2023-08-18 13:55:20 +0700

    Add Upload file Features

--- client/src/components/Upload/Upload.js
+++ client/src/components/Upload/Upload.js
@@ -1,36 +1,47 @@
 import * as React from 'react'  
-
-
+import axios from 'axios'
+import { UserContext } from '../../context/UserContext.jsx';
 
 export default function Upload(){
 
-    const handleFileSelected = () => {
-        
+    const [file, setFile] = React.useState()
+    const {userName,setUserName} = React.useContext(UserContext)
+
+    const handleFileSelected = (event) => {
+        setFile(event.target.files[0])
     }
 
-    const uploadHandler = () => {
-
+    const uploadHandler = (event) => {
+        event.preventDefault()
+        const formData = new FormData()
+        formData.append("file", file)
+        formData.append("userName", userName)
+        axios.post("/api/upload", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then((response) => {
+          console.log(response.data.message);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
-
 
     return(
         <>
-        <div>
-            THis is upload page
-        </div>
-        <div>
-            <h1>Upload your image here</h1>
-            <form>
-                <input type="file" name="image" id="image" onChange={}/>
-                <button onClick={uploadHandler}
-                class="bg-gray-800 w-1/5 text-white">
-                    Upload
-                </button>
-            </form>
-            
-        </div>
-
+            <div>
+                <h1>Upload your image here</h1>
+                <form medthod="POST">
+                    <input type="file" name="image" id="image" onChange={handleFileSelected}/>
+                    <button onClick={uploadHandler}
+                    className="bg-gray-800 w-1/5 text-white">
+                        Upload
+                    </button>
+                </form>
+                
+            </div>
         </>
-
     )
 }
