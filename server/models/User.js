62d70e9d8ede9fb8f01d70d647b056b16092827c
zune <tuanbaonguyen09@gmail.com>
2023-08-24 13:51:50 +0700

    fixing unique

--- server/models/User.js
+++ server/models/User.js
@@ -2,16 +2,8 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    email: {
-      type: String,
-      required: [true, "Email is required"],
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: [true, "Password is required"],
-      unique: false,
-    },
+    email: String,
+    password: String,
   },
   {
     timestamps: true,
